2a0452b3562091194361dde7fc1942d64c5c454b
ida tomoyoshi <tomo080622@gmail.com>
2020-03-28 17:43:53 +0900

    categoryのテーブルも作りました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_113127) do
+ActiveRecord::Schema.define(version: 2020_03_28_080857) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
