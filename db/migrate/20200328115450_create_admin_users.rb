c24867981d19fe1711de4ebddba3cd010b91819f
ura0328 <ura20250103@gmail.com>
2020-03-28 12:15:20 +0000

    #

---
+++ db/migrate/20200328115450_create_admin_users.rb
@@ -0,0 +1,10 @@
+class CreateAdminUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :admin_users do |t|
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
