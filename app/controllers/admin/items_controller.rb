d343c48137dd34c09f364c23fa76847049217c5f
sora-uzu <naruto07170717@gmail.com>
2020-03-30 04:27:42 +0000

    Add Admin_item's controller,view

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,34 @@
+class Admin::ItemsController < ApplicationController
+  def top
+  end
+
+  def index
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+  	@item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item)
+  end
+
+
+  def edit
+  end
+
+
+private
+
+  def item_params
+    params.require(:item).permit(:name, :text, :price, :sell_status)
+  end
+
+
+end
