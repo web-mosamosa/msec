fd70b0ff3d5331ba95790348960f87b813c69092
ida tomoyoshi <tomo080622@gmail.com>
2020-03-31 18:24:50 +0900

    管理の会員側ほぼ完成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,9 +13,9 @@ class Admin::UsersController < ApplicationController
 	end
 
 	def update
-	    user = User.find(params[:id])
-  	if  user.update(user_params)
-  		redirect_to admin_user_path(user)
+	    @user = User.find(params[:id])
+  	if  @user.update(user_params)
+  		redirect_to admin_user_path(@user)
   	else
   		render 'edit'
   	end
