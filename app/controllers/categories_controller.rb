04f3b9f396bea6add688c2d76bac790fced00a56
ida tomoyoshi <tomo080622@gmail.com>
2020-04-02 18:23:30 +0900

    カテゴリーを会員側と管理側で分けた完成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,36 +1,8 @@
 class CategoriesController < ApplicationController
-  def index
-  	@category = Category.all
-  	@new_categories = Category.new
-  	
-  end
   def show
     @categories = Category.all
     @category =Category.find(params[:id])
     @items = @category.items
-  end
-
-  def edit
-  	@categories = Category.find(params[:id])
-  end
-  def create
-  	@categories = Category.all
-  	@category = Category.new(category_params)
-
-  	 @category.save
-      redirect_to categories_path(@categories)
-  end
- 
-
-  def update
-  	@category = Category.find(params[:id])
-        @category.update(category_params)
-        if @category.save
-          redirect_to categories_path(@category.id)
-           else
-          render :edit
-        end
-
   end
    private
     def category_params
