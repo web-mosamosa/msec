da2f49783c6069b36af7e25c6fe8d26b2aa0933b
ida tomoyoshi <tomo080622@gmail.com>
2020-04-05 15:59:23 +0900

    パスワード再設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,13 @@ class UsersController < ApplicationController
 
   def withdraw
     @user = User.find(params[:id])
-
+ end
   def residence
     @residence = Residence.new
     @user = User.find(params[:id])
     @residences = @user.residences
 
   end
-
   def show
   	@user = User.find(params[:id])
   end
@@ -31,7 +30,7 @@ class UsersController < ApplicationController
  
   private
   def user_params
-  	params.require(:user).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postcode, :residence, :phone_number, :email, :user_status)
+  	params.require(:user).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postcode, :residence, :phone_number, :email, :user_status,:password,:password_confirmation)
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-
   devise_for :admins, skip: :all
-
   devise_scope :admin do
     get 'admins/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
     post 'admins/sign_in' => 'admins/sessions#create', as: 'admin_session'
     delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'
   end
-  get '/users/:id/withdraw' => 'users#withdraw'
-  get 'homes/thanks'
   get 'homes/about'
   resources :categories
 
@@ -18,7 +14,10 @@ Rails.application.routes.draw do
   resources :users, only: [:edit,:show,:create,:update,:destroy]
   get 'users/:id/residences' => 'users#residence', as: 'user_residence'
   post 'users/:id/residences' => 'residences#create'
+  devise_scope :user do
+  get 'users/:id/password' => 'users/registrations#edit', as: 'passwordchange'
 
+end
   namespace  :admin do
    resources :users
    resources :items
@@ -36,9 +35,7 @@ Rails.application.routes.draw do
   resources :orders, only: [:new, :index, :show, :create]
   
 
-
   resources :residences
-
 
   resources :items, only: [:index,:show]
   root to: 'items#top'
