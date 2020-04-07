ed8edaa50943da3da15f7a755a9fde26383520e2
ura0328 <ura20250103@gmail.com>
2020-04-07 04:39:18 +0000

    user items layout

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,19 @@
 class ItemsController < ApplicationController
 
 def top
+  @categories = Category.all
+    # urlにcategory_id(params)がある場合
+    if params[:category_id]
+      # Categoryのデータベースのテーブルから一致するidを取得
+      @category = Category.find(params[:category_id])
+       
+      # category_idと紐づく投稿を取得
+      @items = @category.items.order(created_at: :desc).all
+    else
+      # 投稿すべてを取得
+      @items = Item.order(created_at: :desc).all
+    end
+  
 end
 
   def index
@@ -21,6 +34,19 @@ end
   def show
     @item = Item.find(params[:id])
     @cart = Cart.new
+    
+    @categories = Category.all
+    # urlにcategory_id(params)がある場合
+    if params[:category_id]
+      # Categoryのデータベースのテーブルから一致するidを取得
+      @category = Category.find(params[:category_id])
+       
+      # category_idと紐づく投稿を取得
+      @items = @category.items.order(created_at: :desc).all
+    else
+      # 投稿すべてを取得
+      @items = Item.order(created_at: :desc).all
+    end
   end
 
 private
